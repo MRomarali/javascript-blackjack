64038e69dfe91d8fc315444597e7bb6abe9eb05a
Omar Ali <omarabdiali0@gmail.com>
2020-01-12 17:19:51 +0100

    backjack done

--- javascripts.js
+++ javascripts.js
@@ -1,126 +1,100 @@
-//================Generator================================
-/**
- * @function random generator
- * @param {*} min minimum number which in this case is 1
- * @param {*} max maximum number which in this case is 11
- */
+var playerScore = 0; // declared the variabel that stores the users score
+var dealerScore = 0; // declared the variabel that stores the computers score
+/*
+  * @function a function that generates using Math.floor and random function
+  * @param {*} min number to generate which is nr 1
+  * @param {*} max number to generate which is nr 11
+  */
 function randomNum(min, max) {
-    return Math.floor(Math.random() * (11 - 1 + 1) ) + 1
+    randomGen = Math.floor(Math.random() * (11 - 1 + 1) ) + 1
+    return randomNum;
 };
-
-
-let player = 0; //player is the user
-let dealer = 0; // dealer is the opponent aka computer
-let playerScore = 0; // this variabel will store users score
-let dealerScore = 0; // this variabel will store computers score
-
-/**
- * @function includes a forloop that goes 3 rounds bacause our draw limits are just three
- * I use a prompt that asks if user wants to draw. if so, the player draws a card and the score is displayed
- * Then i use if statements with all the conditions.
+​
+/*
+ * @function used to console the users random generated numbers and a console log that stores the score
+ * @param {*} numberGen is a parameter which i use for the random number generator 
  */
-function draw(){
-    for (let i = 0; i < 3; i++) {
-        prompt("Draw a card");
-        player = randomNum();
-        console.log("Player drew " + player);
-        playerScore += player;
-        console.log(playerScore);
-        dealer = randomNum();
-        console.log("Dealer drew " + dealer);
-        dealerScore += dealer;
-        console.log(dealerScore);
-    }
-    if ( dealerScore > 21) {
-        alert("Player win!!!\n Player score " + playerScore + "\n Dealer score " + dealerScore);
-    
-    } else if (playerScore > 21) {
-        alert("Dealer win!!!\n Dealer score " + dealerScore + "\n player score " + playerScore);
-    }
-    else if (dealerScore == 21) {
-        alert("Dealer win!!!\n Dealer score " + dealerScore + "\n player score " + playerScore);
-    } 
-    else if (playerScore == 21) {
-        alert("Player win!!!\n Player score " + playerScore + "\n Dealer score " + dealerScore);
-    }
-    else if (playerScore === dealerScore) {
-        alert("Oavgjort!!!\n Player score " + playerScore + "\n Dealer score " + dealerScore);
-    }
-    else if (playerScore < 21 && dealerScore < 21) {
-        if (playerScore > dealerScore) {
-            alert("Player win!!!\n Player score " + playerScore + "\n Dealer score " + dealerScore);
-        } 
-        else if (dealerScore > playerScore) {
-            alert("Dealer win!!!\n Dealer score " + dealerScore + "\n player score " + playerScore);
-        } 
-    }
-};
-
-/**
- * @function includes a forloop that goes 2 rounds bacause the player already used one draw by skippin and letting opponent draw
- * I use a prompt that asks if user wants to draw. if so, the player draws a card and the score is displayed
- * Then i use if statements with all the conditions.
- */
-function drawAfterStay(game){
-    for (let i = 0; i < 2; i++) {
-        prompt("Draw a card");
-        player = randomNum();
-        console.log("Player drew " + player);
-        playerScore += player;
-        console.log(playerScore);
-        dealer = randomNum();
-        console.log("Dealer drew " + dealer);
-        dealerScore += dealer;
-        console.log(dealerScore);
-    }
-    if ( dealerScore > 21) {
-        alert("Player win!!!\n Player score " + playerScore + "\n Dealer score " + dealerScore);
-    
-    } else if (playerScore > 21) {
-        alert("Dealer win!!!\n Dealer score " + dealerScore + "\n player score " + playerScore);
-    }
-    else if (dealerScore == 21) {
-        alert("Dealer win!!!\n Dealer score " + dealerScore + "\n player score " + playerScore);
-    } 
-    else if (playerScore == 21) {
-        alert("Player win!!!\n Player score " + playerScore + "\n Dealer score " + dealerScore);
-    }
-    else if (playerScore === dealerScore) {
-        alert("Oavgjort!!!\n Player score " + playerScore + "\n Dealer score " + dealerScore);
-    }
-    else if (playerScore < 21 && dealerScore < 21) {
-        if (playerScore > dealerScore) {
-            alert("Player win!!!\n Player score " + playerScore + "\n Dealer score " + dealerScore);
-        } 
-        else if (dealerScore > playerScore) {
-            alert("Dealer win!!!\n Dealer score " + dealerScore + "\n player score " + playerScore);
-        } 
-    }
-};
-
-/**
- * @function does let the dealer draw twice because the player skipped 
- * This displayes dealers draw and score
- *  
- * */
-function stay(stop){
-    let secondDealer = 0;
-    dealer = randomNum();
-    console.log("Dealer have " + dealer);
-    console.log("Dealer have " + dealer);
-    dealer+=dealer;
-    dealerScore += dealer;
-    console.log(dealerScore);
-};
-    
-/**
- * Calling the functions thru an if/ if-else statement, and depending on the users
- * choice the game goes on.
- */
-
-if (prompt("Draw or stay?") == "d") {
- draw();   
-}else if (prompt("Draw or stay?") == "s"){
-    stay();
-    drawAfterStay();
+function playerCard(numberGen){    // here it should get randomGen as a parameter
+    playerScore += randomGen;
+    console.log("player draws " + randomGen);
+    console.log("player score is " + playerScore);
+    return playerScore;
 }
+​
+/*
+ * @function used to console the computers random generated numbers and a console log that stores the score
+ * @param {*} numberGen is a parameter which i use for the random number generator 
+ */
+function dealerCard(numberGen){  // randomGen as param
+    dealerScore += randomGen;
+    console.log("dealer draws " + randomGen);
+    console.log("dealer score is " + dealerScore);
+    return dealerScore;
+}
+​
+/*
+ * @function is a func that has if statements in it with the games conditions
+ */
+function winner(){
+    if(playerScore==21 && dealerScore!=21) {
+        alert("Player win!!!\n Player score " + playerScore + "\n Dealer score " + dealerScore);
+    }
+​
+    else if(playerScore>21) {           
+        alert("Dealer win!!!\n Dealer score " + dealerScore + "\n player score " + playerScore);
+    } 
+​
+    else if(dealerScore>21){         
+        alert("Player win!!!\n Player score " + playerScore + "\n Dealer score " + dealerScore);
+    }
+​
+    else if(playerScore!=21 && dealerScore==21){         
+        alert("Dealer win!!!\n Dealer score " + dealerScore + "\n player score " + playerScore);
+    }
+}
+/*
+ * @function play is created just to hold the two other functions
+ * and then i call this function to display these functions.
+ */
+function play(){
+playerCard(randomNum());
+dealerCard(randomNum());
+dealerCard(randomNum());
+}
+​
+​
+/*
+ * @function made with if statements. These if/ else if statements
+ * reacts when the s or d keyboards is clicked.
+ * 
+ * @param {*} key holds keyboard input that the users
+ * press which in this case is 's' and 'd'
+ * 
+ */
+window.addEventListener("keypress",function (evt){
+    if (evt.code == "KeyD")//D 
+    {
+        if (playerScore == 0 && dealerScore == 0){
+            console.log("Player draws a new card");
+            playerCard(randomNum());
+            dealerCard(randomNum());
+            dealerCard(randomNum());
+        }else{
+            playerCard(randomNum());
+        }
+        winner();
+    }
+    else if (evt.code == "KeyS")//S
+    {
+        if (dealerScore < 17) {
+            dealerCard(randomNum());
+        }
+        if (playerScore === dealerScore) {
+            alert("It\'s a draw!!!\n Player score " + playerScore + "\n Dealer score " + dealerScore);
+        }else if (playerScore > dealerScore){
+​           alert("Player win!!!\n Dealer score " + dealerScore + "\n player score " + playerScore); 
+        }else{
+​           alert("Dealer win!!!\n Dealer score " + dealerScore + "\n player score " + playerScore);
+        }
+    }
+});
